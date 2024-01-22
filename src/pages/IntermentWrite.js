9438df3657d9bc3fc478571589943518a88b0f99
HyungJu19 <HyungJu911@gmail.com>
2024-01-22 15:03:35 +0900

    기능 강화 및 새 페이지 추가
    - 패키지 설정 및 메인 인덱스 파일 업데이트
    - 관리자, 홈, 마이 페이지 및 각종 상세 페이지 추가
    - 사용자 인증 개선을 위한 새 로그인 페이지 구현

---
+++ src/pages/IntermentWrite.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IntermentWrite = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default IntermentWrite;
---
+++ src/pages/StandByPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StandByPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default StandByPage;
