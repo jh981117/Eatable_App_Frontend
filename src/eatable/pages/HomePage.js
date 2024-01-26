4343fa59e4bfae14a05cf2e76d794e89da862f77
송민호 <hyungju911@gmail.com>
2024-01-26 16:26:25 +0900

    민호 진무 머지

--- src/eatable/pages/HomePage.js
+++ src/eatable/pages/HomePage.js
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
-
-
-
-
+import React from "react";
 
 const HomePage = () => {
-
-    const [state, setState] = useState([]);
-  return (
-    <div>
-      <div>
-        123123123123123123213123213123123
-        <div>
-          <div></div>
-          <div></div>
-        </div>
-      </div>
-    </div>
-  );
-
-
-  
- 
-
-
-
+  return <div></div>;
 };
 
 export default HomePage;
