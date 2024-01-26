3ebb7bf6f418f547227ab91a0789c0c48b1a5568
송민호 <hyungju911@gmail.com>
2024-01-26 17:28:13 +0900

    기본틀 완성 민호

--- src/eatable/EatableApp.js
+++ src/eatable/EatableApp.js
@@ -1,2 +1,19 @@
 import React from "react";
-rsc;
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const EatableApp = () => {
+  return (
+    <div>
+      <Container>
+        <Routes>
+          <Route path="/" Component={HomePage}></Route>
+          <Route path="/home" Component={HomePage}></Route>
+        </Routes>
+      </Container>
+    </div>
+  );
+};
+
+export default EatableApp;
