feb41c239b6cb2aafc85c52e99554e23ec26333c
seungbin <imsen456@gmail.com>
2024-01-28 21:17:44 +0900

    승빈 기본폼

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EatableApp from "./eatable/EatableApp";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <EatableApp />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
