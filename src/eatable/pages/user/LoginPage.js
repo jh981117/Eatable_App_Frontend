e3d12b38bf54b0708b6014aa3ff15a9a574be74e
송민호 <hyungju911@gmail.com>
2024-01-31 02:25:31 +0900

    민호 token

--- src/eatable/pages/user/LoginPage.js
+++ src/eatable/pages/user/LoginPage.js
@@ -1,115 +1,154 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useNavigate, useResolvedPath } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = (props) => {
+const LoginPage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();     // 페이지간 이동을 담당하는 함수 생성
+  const [user, setUser] = useState({
+    id: "",
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: "",
+    submitError: "",
+  });
 
-    const [user, setUser] = useState({  // user상태 초기화(const로 정의 및 초기화)
-        id: "",                         // useState : react의 상태관리훅(업데이트하는 함수 setuser생성)
-        username: "",
-        password: "",
+  const changeValue = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
-    console.log(user);  // user를 잘 받아오는지 콘솔창에서 확인
+  };
 
-    // useState훅을 사용 각 필드에대한 오류 메시지를 빈 문자열로 설정.
-    // const [idError, setIdError] = useState('');
-    const [usernameError, setUsernameError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+  const submitUser = async (e) => {
+    e.preventDefault();
 
-    // 제출 과정에서 발생하는 오류 메시지를 저장
-    const [submitError, setSubmitError] = useState('');     
+    setUser({
+      usernameError: "",
+      passwordError: "",
+      submitError: "",
+    });
 
-    // 유효성 검사 함수
-    const validateField = (fieldName, value) => {
-        switch (fieldName) {
-            case 'username':
-                return value.trim() === '' ? '아이디를 입력해주세요.' : ((value === setUser.username) ? '' : '존재하지 않는 아이디 입니다.');     // 저장된 아이디인지도 확인해줘야함. 실제로 있는 아이디인지.
-            case 'password':
-                return value.trim() === '' ? '비밀번호를 입력해주세요.' : ((value === setUser.password) ? '' : '비밀번호를 확인해주세요.');
-        }
-    };
+    const usernameError = validateField("username", user.username);
+    const passwordError = validateField("password", user.password);
 
-    const changeValue = (e) => {
-        setUser({
-            ...user,
-            [e.target.name] : e.target.value,
+    setUser({
+      ...user,
+      usernameError: usernameError,
+      passwordError: passwordError,
+    });
+
+    if (!usernameError && !passwordError) {
+      try {
+        const response = await fetch("http://localhost:8080/api/authenticate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: user.username,
+            password: user.password,
+          }),
         });
-    };
 
-    const submitUser = (e) => {
-        e.preventDefault();
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log("로그인 성공", data);
+          alert("로그인 성공!");
 
-        // < 에러상태 초기화 >
-        setUsernameError('');
-        setPasswordError('');
+          saveTokenToLocalStorage(data.token); // JWT 저장
 
-        // < 필드에 대한 유효성 >
-        const usernameError = validateField('username', user.username);
-        const passwordError = validateField('password', user.password);
-
-        // < 에러메시지 >
-        setUsernameError(usernameError);
-        setPasswordError(passwordError);
-
-        if (!usernameError && !passwordError) {
-            // fetch("http://localhost:8080/user/login", {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-Type': 'application/json;charset=utf-8',
-            //     },
-            //     body: JSON.stringify(user),
-            // })
-            // .then(response => {
-            //     console.log(`response`, response);
-            //     if (response.status === 201) {
-            //         return response.json();                    
-            //     } else {
-            //         return null;
-            //     }
-            // })
-            // .then(data => {
-            //     if (data !== null) {
-            //         console.log(`로그인 성공`, data);
-            //         alert("로그인 성공!");
-            //         navigate(`home`);
-            //     } else {
-            //         alert("로그인 실패!");
-            //     }
-            // });
-            alert("로그인 성공!");
-            navigate(`home`);
+          navigate(`/applyreq`);
+        } else {
+          console.error("로그인 실패:", response.status);
+          alert("로그인 실패!");
         }
-    };
-
-    const signup = () => {
-        navigate("/signup/");
+      } catch (error) {
+        console.error("로그인 요청 중 오류 발생:", error);
+        alert("로그인 요청 중 오류 발생!");
+      }
     }
+  };
 
-    return (
-        <div>
-            <h2>로그인 페이지</h2>
+  const validateField = (fieldName, value) => {
+    switch (fieldName) {
+      case "username":
+        return value.trim() === ""
+          ? "아이디를 입력해주세요."
+          : value === user.username
+          ? ""
+          : "존재하지 않는 아이디 입니다."; // 저장된 아이디인지도 확인해줘야함. 실제로 있는 아이디인지.
+      case "password":
+        return value.trim() === ""
+          ? "비밀번호를 입력해주세요."
+          : value === user.password
+          ? ""
+          : "비밀번호를 확인해주세요.";
+      default:
+        return "";
+    }
+  };
 
-            <Form onSubmit={submitUser}>
-                <Form.Group className="mt-3" controlId="formBasicUsername">
-                    <Form.Label>아이디 : </Form.Label>
-                    <Form.Control type="text" name="username" placeholder="아이디를 입력해주세요." value={user.username} onChange={changeValue}/>
-                    {usernameError && <div className="text-danger">{usernameError}</div>}
-                </Form.Group>
+  const signup = () => {
+    navigate("/signup");
+  };
 
-                <Form.Group className="mt-3" controlId="formBasicPassword">
-                    <Form.Label>비밀번호 : </Form.Label>
-                    <Form.Control type="password" name="password" placeholder="비밀번호를 입력해주세요." value={user.password} onChange={changeValue}/>
-                    {passwordError && <div className="text-danger">{passwordError}</div>}
-                </Form.Group>
+  const saveTokenToLocalStorage = (token) => {
+    // 로컬 스토리지에 토큰 저장 로직을 여기에 구현
+    localStorage.setItem("token", token);
+  };
 
-                {submitError && <div className="text-danger">{submitError}</div>}
-                <Button variant="primary" type="submit" onClick={submitUser}>로그인</Button>
-                <Button variant="primary" type="submit" onClick={signup}>회원가입</Button>
-            </Form>
-        </div>
-    );
+  return (
+    <Container className="mt-3 col-6 flex justify-content-center">
+      <h2>로그인 페이지</h2>
+
+      <Form onSubmit={submitUser}>
+        <Form.Group className="mt-3" controlId="formBasicUsername">
+          <Form.Label>아이디 : </Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            placeholder="아이디를 입력해주세요."
+            value={user.username}
+            onChange={changeValue}
+          />
+          {user.usernameError && (
+            <div className="text-danger">{user.usernameError}</div>
+          )}
+        </Form.Group>
+
+        <Form.Group className="mt-3" controlId="formBasicPassword">
+          <Form.Label>비밀번호 : </Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요."
+            value={user.password}
+            onChange={changeValue}
+          />
+          {user.passwordError && (
+            <div className="text-danger">{user.passwordError}</div>
+          )}
+        </Form.Group>
+
+        {user.submitError && (
+          <div className="text-danger">{user.submitError}</div>
+        )}
+        <Button variant="primary" type="submit">
+          로그인
+        </Button>
+        <Button
+          className="m-2"
+          variant="primary"
+          type="button"
+          onClick={signup}
+        >
+          회원가입
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default LoginPage;
