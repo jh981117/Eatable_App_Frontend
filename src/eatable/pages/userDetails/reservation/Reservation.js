14ff8be9886a7dce9fd8df066f7019295fad3cba
재환 <jh981117@naver.com>
2024-02-01 17:26:05 +0900

    캘린더 라이브러리 추가 재환

--- src/eatable/pages/userDetails/reservation/Reservation.js
+++ src/eatable/pages/userDetails/reservation/Reservation.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate, useParams } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { setHours, setMinutes, format } from 'date-fns';
 
 const Reservation = () => {
     const navigate = useNavigate();
-
-    let {id} = useParams();
+    let { id } = useParams();
 
     const [showGreeting, setShowGreeting] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(
+        setHours(setMinutes(new Date(), 30), 16)
+    );
+
+    const handleChangeDate = (date) => {
+        setSelectedDate(date);
+    };
 
     const goReservationOk = () => {
         setShowGreeting(true);
@@ -15,22 +24,47 @@ const Reservation = () => {
         // navigate("/reservationOk");
     };
 
+    const showReservationOk = () => {
+        navigate("/reservationOk/" + {id})
+    };
+
     return (
         <div>
             예약 인원 설정하기 <br />
             성인 <br />
-            유아
+
             <hr />
-            총원
+
+
             <Button onClick={goReservationOk}>다음</Button> <br />
             {showGreeting && (
                 <>
-                    ㅎㅇㅎㅇ
+                예약일 설정 <br/>
+                {/* Integrate the DatePicker */}
+                <DatePicker
+                    selected={selectedDate}
+                    onChange={handleChangeDate}
+                    showTimeSelect
+                    includeTimes={[
+                        setHours(setMinutes(new Date(), 0), 17),
+                        setHours(setMinutes(new Date(), 30), 18),
+                        setHours(setMinutes(new Date(), 30), 19),
+                        setHours(setMinutes(new Date(), 30), 17),
+                    ]}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                />
                     <br />
                     <div className='text-center'>
-                    <Button style={{ fontSize: '1.5rem', marginTop: '1rem', width: '25rem' }} onClick={() => navigate("/reservationOk/")}>
-                        예약확정
-                    </Button>
+                        <Button
+                            style={{
+                                fontSize: '1.5rem',
+                                marginTop: '1rem',
+                                width: '25rem',
+                            }}
+                            onClick={showReservationOk}
+                        >
+                            예약확정
+                        </Button>
                     </div>
                 </>
             )}
