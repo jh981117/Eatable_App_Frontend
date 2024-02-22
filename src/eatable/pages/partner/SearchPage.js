fb3047f1a4310d497a5d9298340f56b6157a479c
chaiginmoo <chaiginmoo@gmail.com>
2024-02-22 15:33:40 +0900

    수정
    수정완료

--- src/eatable/pages/partner/SearchPage.js
+++ src/eatable/pages/partner/SearchPage.js
@@ -64,7 +64,9 @@ const SearchPage = () => {
         if (page === 0) {
           setPartners(data.content);
         } else {
-          setPartners((prevPartners) => [...prevPartners, ...data.content]);
+          setPartners((prevPartners) => [...prevPartners, ...data.content.filter(
+            (partner) => !prevPartners.some((p) => p.id === partner.id)
+          )]);
         }
 
 
@@ -96,84 +98,84 @@ const SearchPage = () => {
         <AutoComplete className=" mb-3" onAutoCompleteData={handleInputChange} />
 
         <div className="image-grid " >
-          <div className="image-container" onClick={() => setInputValue("족발.보쌈")}>
+          <div className="image-container" onClick={() => handleInputChange("족발.보쌈")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708392798299-free-icon-jokbal-8740559.png"
               className="Icon"
             />
             <span className="keyword">족발.보쌈</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("돈까스")} >
+          <div className="image-container" onClick={() => handleInputChange("돈까스")} >
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708392859189-free-icon-food-12711337.png"
               className="Icon"
             />
             <span className="keyword">돈까스</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("고기.구이")} >
+          <div className="image-container" onClick={() => handleInputChange("고기.구이")} >
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708392885461-free-icon-grilled-meat-1791773.png"
               className="Icon"
             />
             <span className="keyword">고기.구이</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("피자")} >
+          <div className="image-container" onClick={() => handleInputChange("피자")} >
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708392913319-free-icon-pizza-2497913.png"
               className="Icon"
             />
             <span className="keyword">피자</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("찜.탕.찌개")}>
+          <div className="image-container" onClick={() => handleInputChange("찜.탕.찌개")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708395221599-free-icon-soup-8065267.png"
               className="Icon"
             />
             <span className="keyword">찜.탕.찌개</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("양식")} >
+          <div className="image-container" onClick={() => handleInputChange("양식")} >
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393241132-free-icon-spaguetti-3480618.png"
               className="Icon"
             />
             <span className="keyword">양식</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("중식")} >
+          <div className="image-container" onClick={() => handleInputChange("중식")} >
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393266825-free-icon-jajangmyeon-2090214.png"
               className="Icon"
             />
             <span className="keyword">중식</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("아시안")}>
+          <div className="image-container" onClick={() => handleInputChange("아시안")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393286213-free-icon-wonton-noodles-7593714.png"
               className="Icon"
             />
             <span className="keyword">아시안</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("치킨")}>
+          <div className="image-container" onClick={() => handleInputChange("치킨")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393306212-free-icon-chicken-6679109.png"
               className="Icon"
             />
             <span className="keyword">치킨</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("한식")}>
+          <div className="image-container" onClick={() => handleInputChange("한식")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393325091-free-icon-rice-bowl-5990470.png"
               className="Icon"
             />
             <span className="keyword">한식</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("버거")}>
+          <div className="image-container" onClick={() => handleInputChange("버거")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393341765-free-icon-beef-1720541.png"
               className="Icon"
             />
             <span className="keyword">버거</span>
           </div>
-          <div className="image-container" onClick={() => setInputValue("분식")}>
+          <div className="image-container" onClick={() => handleInputChange("분식")}>
             <Image
               src="https://eatablebucket.s3.ap-northeast-2.amazonaws.com/1708393359686-free-icon-rice-cake-9745081.png"
               className="Icon"
